840afc117b7bb05295068b9742f8e5dbfec70389
kmgovind <kavin@liquidrocketry.com>
2022-06-17 10:18:22 -0400

    Created lead-calculation branch

--- lead-calculation/simulation.cpp
+++ lead-calculation/simulation.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+/* CODE PLAN
+
+Coordinate System: Turret-centric
+Turret Attributes:
+    - Center of coordinate system
+    - Movement Velocity/Direction (package as a vector)
+Target Attributes:
+    - Position vector
+    - Velocity vector
+
+Simulation Parameters:
+    - Timestep: 20ms (0.020s)
+
+*/
 
 int main(){
     cout << "Hello World!" << endl;
